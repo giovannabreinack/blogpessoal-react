cdb3588e355120e101b030238a5aa369cd66b9ca
Gih <giovannabreinackcolombara@gmail.com>
2025-07-28 14:10:42 -0300

    adicionando página de login

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Cadastro from './pages/cadastro/Cadastro'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
-        <div className="min-h-80vh]">
+        <div className="min-h-[80vh]">
           <Routes>
-            {/* <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} /> */}
-            <Route path="/" element={<Cadastro />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
         <Footer />
