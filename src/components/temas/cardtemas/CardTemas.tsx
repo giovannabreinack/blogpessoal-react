8f997921747c4db69dafad77ba37ebb07c0b05f0
Gih <giovannabreinackcolombara@gmail.com>
2025-07-30 12:26:28 -0300

    atualização na cor do formulario de tema

--- src/components/temas/cardtemas/CardTemas.tsx
+++ src/components/temas/cardtemas/CardTemas.tsx
@@ -15,7 +15,7 @@ function CardTemas({ tema }: CardTemasProps) {
             <p className='p-8 text-3xl bg-slate-200 h-full'>{tema.descricao}</p>
             <div className="flex">
                 <Link to={`/editartema/${tema.id}`}
-                    className='w-full text-slate-100 bg-	indigo-400 hover:bg-indigo-800 
+                    className='w-full text-slate-100 bg-sky-700 hover:bg-sky-900
     flex items-center justify-center py-2'>
                     <button>Editar</button>
                 </Link>
