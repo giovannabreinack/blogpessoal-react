8c3d7dc01061aea632d2d1c186cb6b161928f2c1
Gih <giovannabreinackcolombara@gmail.com>
2025-07-30 12:03:43 -0300

    adicionando a lógica nas classes de tema

--- src/components/temas/cardtemas/CardTemas.tsx
+++ src/components/temas/cardtemas/CardTemas.tsx
@@ -1,13 +1,18 @@
-import { Link } from 'react-router-dom'
 
-function CardTemas() {
+import { Link } from 'react-router-dom'
+import type Tema from '../../../models/Tema'
+
+interface CardTemasProps {
+    tema: Tema
+}
+
+function CardTemas({ tema }: CardTemasProps) {
     return (
         <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
             <header className='py-2 px-6 bg-sky-700 text-white font-bold text-2xl'>
                 Tema
             </header>
-            <p className='p-8 text-3xl bg-slate-200 h-full'>Descrição</p>
-
+            <p className='p-8 text-3xl bg-slate-200 h-full'>{tema.descricao}</p>
             <div className="flex">
                 <Link to=''
                     className='w-full text-slate-100 bg-sky-700 hover:bg-sky-900
