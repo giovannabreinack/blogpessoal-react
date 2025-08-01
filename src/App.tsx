7cde179a18121e29f2e6435602f20a2a407b5d57
Gih <giovannabreinackcolombara@gmail.com>
2025-08-01 18:30:13 -0300

    adicionando estilização no formulario de postagens

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import ListaPostagens from './components/postagens/listapostagens/ListaPostagens'
 import DeletarTema from './components/temas/deletartema/DeletarTema'
 import FormTema from './components/temas/formtema/FormTema'
 import ListaTemas from './components/temas/listatemas/ListaTemas'
@@ -9,7 +10,7 @@ import { AuthProvider } from './contexts/AuthContext'
 import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-import ListaPostagens from './components/postagens/listapostagens/ListaPostagens'
+import FormPostagem from './components/postagens/formpostagem/FormPostagem'
 
 function App() {
   return (
@@ -28,6 +29,8 @@ function App() {
               <Route path="/editartema/:id" element={<FormTema />} />
               <Route path="/deletartema/:id" element={<DeletarTema />} />
               <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+              <Route path="/editarpostagem/:id" element={<FormPostagem />} />
             </Routes>
           </div>
           <Footer />
