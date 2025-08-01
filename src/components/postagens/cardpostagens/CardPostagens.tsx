be4dcd19121e1ceb67a3e1589e6df50685b48951
Gih <giovannabreinackcolombara@gmail.com>
2025-08-01 18:54:04 -0300

    adicionando lógica na tela de deletar postagem

--- src/components/postagens/cardpostagens/CardPostagens.tsx
+++ src/components/postagens/cardpostagens/CardPostagens.tsx
@@ -37,8 +37,9 @@ function CardPostagens({ postagem }: CardPostagensProps) {
     flex items-center justify-center py-2'>
                     <button>Editar</button>
                 </Link>
-                <Link to='' className='text-white bg-red-400 
-                    hover:bg-red-700 w-full flex items-center justify-center'>
+                <Link to={`/deletarpostagem/${postagem.id}`}
+                    className='text-white bg-red-400 
+	hover:bg-red-700 w-full flex items-center justify-center'>
                     <button>Deletar</button>
                 </Link>
             </div>
