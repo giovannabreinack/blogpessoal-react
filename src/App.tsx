0135f8f9a02691c2887b91eeebadc412a6e9f7d1
Gih <giovannabreinackcolombara@gmail.com>
2025-08-01 19:02:16 -0300

    adicionado estilização e lógica na tela de perfil

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem'
 import FormPostagem from './components/postagens/formpostagem/FormPostagem'
 import ListaPostagens from './components/postagens/listapostagens/ListaPostagens'
 import DeletarTema from './components/temas/deletartema/DeletarTema'
@@ -11,7 +12,7 @@ import { AuthProvider } from './contexts/AuthContext'
 import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem'
+import Perfil from './pages/perfil/Perfil'
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
               <Route path="/cadastrarpostagem" element={<FormPostagem />} />
               <Route path="/editarpostagem/:id" element={<FormPostagem />} />
               <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
