6583a1f3fb908e26162cb2081c7816774bdb4782
Gih <giovannabreinackcolombara@gmail.com>
2025-08-05 10:33:40 -0300

    alterando a lógica de exibição do footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,19 @@
 import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { useContext, type ReactNode } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
 
     const data = new Date().getFullYear()
 
-    return (
-        <>
+    const { usuario } = useContext(AuthContext)
+
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+
             <div className="flex justify-center bg-gradient-to-r from-sky-700 via-purple-500 to-fuchsia-500 text-white font-serif text-shadow-lg">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
@@ -25,6 +33,11 @@ function Footer() {
                     </div>
                 </div>
             </div>
+        )
+    }
+    return (
+        <>
+            {component}
         </>
     )
 }
